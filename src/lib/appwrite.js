a68ff4099db894b6083190c9b7733e8e6edaa70f
KhoaTran160401 <tranvankhoa.ag.01@gmail.com>
2025-09-04 15:37:38 +0700

    first commit

---
+++ src/lib/appwrite.js
@@ -0,0 +1,14 @@
+import { Client, Account, Databases, Storage } from "appwrite";
+
+const client = new Client()
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+
+const account = new Account(client);
+const databases = new Databases(client);
+const storage = new Storage(client);
+const tables = {
+  PRODUCTS: "products"
+}
+
+export { client, account, databases, storage, tables };
